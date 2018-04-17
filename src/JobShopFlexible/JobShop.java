a5e443f1912b6dde96981ed491cfa4b16dfc2dce
MathieuRaynaud <mraynaud@etud.insa-toulouse.fr>
2018-04-17 10:24:06 +0200

    Traitement des donnees et inclusion des tests

--- src/JobShopFlexible/JobShop.java
+++ src/JobShopFlexible/JobShop.java
@@ -1,13 +1,59 @@
 package JobShopFlexible;
 
+import java.io.*;
+
 public class JobShop {
 
     public Graphe JobShopGraph ;
     public int nbMachines;
     public Machine[] Machines;
 
-    public JobShop (int nbMachines){
-        this.JobShopGraph = new Graphe(10);
+    public JobShop (File file){
+        BufferedReader br = null;
+        FileReader fr = null;
+        Integer currentLine = 0;
+
+        try {
+
+            //br = new BufferedReader(new FileReader(FILENAME));
+            fr = new FileReader(file);
+            br = new BufferedReader(fr);
+            String sCurrentLine;
+
+            while ((sCurrentLine = br.readLine()) != null) {
+                if(sCurrentLine.charAt(0) == ' ') sCurrentLine = sCurrentLine.substring(1, sCurrentLine.length());
+                String[] parts = sCurrentLine.split("[ \\t]+");
+                if(currentLine == 0){
+                    System.out.println("Nombre de proc : " + parts[0]);
+                    System.out.println("Nombre de machines : " + parts[1]);
+                    System.out.println("Nombre de machines necessaires : " + parts[2]);
+                    System.out.println("\n*-----------------------------------*");
+                }
+                else{
+                    Integer nbActivite = 1;
+                    int indice = 1;
+                    int i, nbMachines = 0;
+                    System.out.println("\nProcessus " + currentLine + " : " + parts[0] + " machines.");
+                    while(indice < parts.length){
+                        String numActivite = currentLine.toString() + '.' + nbActivite.toString();
+                        System.out.println("  Activité " + numActivite + " - Machine(s) nécessaire(s)  : " + parts[indice]);
+                        nbMachines = Integer.parseInt(parts[indice]);
+                        i = indice+1;
+                        while(i <= indice+nbMachines*2){
+                            System.out.println("    Machine " + parts[i] + " - Duree : " + parts[i+1]);
+                            i += 2;
+                        }
+                        indice = i;
+                        nbActivite++;
+                    }
+                    System.out.println("\n*-----------------------------------*");
+                }
+                currentLine++;
+            }
+
+        } catch (IOException | IndexOutOfBoundsException err) {}
+
+        /*this.JobShopGraph = new Graphe(10);
         this.nbMachines = nbMachines;
 
         //Initialisation des sommets
@@ -63,10 +109,12 @@ public class JobShop {
         this.Machines = new Machine[nbMachines];
         for (int i=0; i<this.nbMachines; i++){
             Machines[i] = new Machine(i);
-        }
+        }*/
     }
 
-
-
+    public static void main (String[] args){
+        File file = new File("Job_Data/Barnes/Text/mt10c1.fjs");
+        JobShop JobFlex = new JobShop(file);
+    }
 
 }
